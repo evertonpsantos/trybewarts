2cb7c7481797b56699decf24668058da83c8ed79
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-13 18:07:50 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const password = document.querySelector('.password');
 const buttonLogIn = document.getElementById('start-btn');
 const textArea = document.getElementById('textarea');
 const counter = document.getElementById('counter');
+const evaluationForm = document.getElementById('evaluation-form');
+const formDataSpace = document.getElementById('form-data-space');
+const firstName = document.getElementById('input-name');
+const lastName = document.getElementById('input-lastname');
+const formEmail = document.getElementById('input-email');
+const house = document.getElementById('house');
+const family = document.getElementsByName('family');
+const subject = document.getElementsByClassName('subject');
+const rating = document.getElementsByName('rate');
 
 function warnMessage() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -37,4 +46,50 @@ textArea.addEventListener('input', () => {
   countCharacters();
 });
 
-// requisito 20 ^
+const familyValue = () => {
+  for (let i = 0; i < family.length; i += 1) {
+    if (family[i].checked) {
+      return family[i].value;
+    }
+  }
+};
+
+const subjectValue = () => {
+  const aux = [];
+  for (let i = 0; i < subject.length; i += 1) {
+    if (subject[i].checked) {
+      aux.push(subject[i].value);
+    }
+  }
+  const newAux = aux.join(', ');
+  return newAux;
+};
+
+const ratingValue = () => {
+  for (let i = 0; i < rating.length; i += 1) {
+    if (rating[i].checked) {
+      return rating[i].value;
+    }
+  }
+};
+
+const formDataCreate = () => {
+  const formData = document.createElement('form');
+  formData.id = 'form-data';
+  const dataInfo = `Nome: ${firstName.value} ${lastName.value};
+  Email: ${formEmail.value};
+  Casa: ${house.value};
+  Família: ${familyValue()};
+  Matérias: ${subjectValue()};
+  Avaliação: ${ratingValue()};
+  Observações: ${textArea.value};
+  `;
+  formData.innerText = dataInfo;
+  formDataSpace.appendChild(formData);
+};
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  evaluationForm.style.display = 'none';
+  formDataCreate();
+});
